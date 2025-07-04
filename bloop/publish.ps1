3d7d43938e90fa50e613280c15d147e4457973fd
Barry Dorrans <github.com@idunno.org>
2025-07-04 13:06:54 -0700

    Add architecture renaming

--- bloop/publish.ps1
+++ bloop/publish.ps1
@@ -1,2 +1,7 @@
 dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true -o app/x64
 dotnet publish -c Release -r win-arm64 -p:PublishSingleFile=true --self-contained true -o app/arm64
+
+Get-ChildItem "app\" "*.exe" -Recurse |
+foreach-object {
+    Rename-Item -Path $_.FullName "$($_.BaseName)-$($_.Directory.Name)$($_.Extension)"
+}
